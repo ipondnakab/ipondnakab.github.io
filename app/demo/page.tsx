348475a2ba85b2ea37be90bdf263f9e043122093
Pond <kittipat_dd@kkumail.com>
2023-12-13 03:30:36 +0700

    add: WIP page

---
+++ app/demo/page.tsx
@@ -0,0 +1,8 @@
+import WorkingInProgress from "@/components/WIP/WorkingInProgress";
+import React from "react";
+
+const Demo: React.FC = () => {
+  return <WorkingInProgress />;
+};
+
+export default Demo;
---
+++ components/WIP/WorkingInProgress.tsx
@@ -0,0 +1,21 @@
+import { Card } from "@nextui-org/react";
+import React from "react";
+import { IoCodeWorking } from "react-icons/io5";
+
+const WorkingInProgress: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-[calc(100vh-4rem)] p-4">
+      <Card isBlurred className="p-4 items-center justify-center py-8">
+        <IoCodeWorking className="text-9xl mb-4" />
+        <div className="flex items-center justify-center gap-4">
+          <h2 className="text-2xl font-bold">WIP</h2>
+          <p className=" border-l border-gray-400 py-3 pl-4 pr-0">
+            Work in progress.
+          </p>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default WorkingInProgress;
