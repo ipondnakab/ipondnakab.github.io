cb4614f597d9741d83b93abe241e578d6f9a9c71
Pond <pond@odds.team>
2026-07-29 22:20:46 +0700

    fix: adjust default size and height calculation for FlagIcon component

--- components/layouts/FlagIcon.tsx
+++ components/layouts/FlagIcon.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const FlagIcon: React.FC<{ src: string; size?: number }> = ({
   src,
-  size = 22,
+  size = 20,
 }) => (
   <Image
     src={src}
     alt=""
     radius="none"
     width={size}
-    height={Math.round((size * 2) / 3)}
+    height={size}
     className="object-cover shadow-sm"
   />
 );
